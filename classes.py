17873bd92f9fda0d23bf9de087d24a136c009bf5
BuildTools <lemerlin.lemerlin@gmail.com>
2023-01-17 23:45:24 +0100

    Commentaire des derniers ajout et réecriture pour utiliser le match

--- classes.py
+++ classes.py
@@ -1,25 +1,39 @@
+
+#Récupére les attaques disponibles en fonction d'une classe (texte)
+#Retourne un tuple qui contient un tuple par attaque sous la forme
+#("NomCompetence", mana, num, "Description", ("Type"))
 def attacks(classe) -> tuple:
-    if classe == "berserker":
-        return (
+    match classe:
+        case "berserker":
+            return (
             ("Psychotic Style", 8, 10, "The berserker loses control, dealing 10 damage", ("att")),
             ("Unbreakable Will", 12, 20, "The berserker channels their inner fury, increasing their defense by 20 points", ("def")),
             ("Raging Storm", 15, 15, "The berserker unleashes a fierce storm of attacks, dealing 15 damage", ("att")),
             ("Bloodlust", 30, 25, "The berserker enters a frenzied state, increasing their health and dealing damage by 25 points", ("hp", "att"))
-        )
-    elif classe == "mage":
-        return (
+            )
+        case "mage":
+            return (
             ("Fireball", 8, 10, "The mage throws a ball of fire, dealing 10 damage", ("att")),
             ("Ice Barrier", 12, 15, "The mage creates a barrier of ice, increasing their defense by 15 points", ("def")),
             ("Lightning Bolt", 15, 15, "The mage calls down a bolt of lightning, dealing 15 damage", ("att")),
             ("Mana Drain", 20, 20, "The mage drains the mana of their opponent, restoring their own mana by 20 points", ("mana","attmana"))
-        )
-
+            )
+        
+#Recupére les stats de base d'un calsse en fonction du nom de cette classe
+#Retourne un dictionnaire sous la forme
+#{"mana": NumMana, "hp": NumHp, "def": NumDef, "topMana": NumTopMana}
 def stats(classe):
-    if classe == "berserker":
-        return {"mana":0,"hp":100,"def":20,"topMana":40}
-    elif classe == "mage":
-        return {"mana":10,"hp":80,"def":10,"topMana":80}
+    match classe:
+        case "berseker":
+            return {"mana":0,"hp":100,"def":20,"topMana":40}
+        case "mage":
+            return {"mana":10,"hp":80,"def":10,"topMana":80}
+        
 
-berserkers = {"att":attacks("berserker"),"stats":stats("berserker")}
-mages = {"att":attacks("mage"),"stats":stats("mage")}
-print(berserkers)
+#A supprimer car inutile ?
+berserkers = {"att": attacks("berserker"),"stats": stats("berserker")}
+mages = {"att": attacks("mage"),"stats": stats("mage")}
+
+
+
+#print(berserkers)
--- core.py
+++ core.py
@@ -1,8 +1,6 @@
 from random import randint
+from classes import *
 
-#Stocke les différentes statistiques de chaque sous la forme:
-# "nom": [MaxMana, def]
-Classes =  {"none" : [50, 10]}
 
 # Permet de lancer un nombre donné de dés
 # Retourne une liste contenant les résultats des lancer
@@ -17,11 +15,3 @@ def roll_dice(dice_number):
 # Retourne entre 1 et 6
 def dice():
     return randint(1, 6)
-
-
-def competence(number, player, competence_type):
-    match competence_type:
-        case "att":
-            print("test")
-        case "def":
-            print(2)
